07227ad0ae2611a1daf0ee26f139c9205dbb5672
Tyler Weaver <maybe@tylerjw.dev>
2022-11-15 09:53:18 -0700

    Rebase from 'release/rolling/rsl'

--- include/rsl/overload.hpp
+++ include/rsl/overload.hpp
@@ -2,6 +2,13 @@
 
 namespace rsl {
 
+/** @file */
+
+/**
+ * @brief Class template for creating overloads sets to use with std::visit
+ *
+ * @tparam Ts Types
+ */
 template <typename... Ts>
 struct Overload : Ts... {
     using Ts::operator()...;
